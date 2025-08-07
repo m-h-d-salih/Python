05525555d7bea434c8232efff34b62e9687ef0e7
Mohammed Salih k <salihsha656@gmail.com>
2025-08-07 11:03:36 +0530

    arithematic

---
+++ operators.py
@@ -0,0 +1,22 @@
+#operators
+# arithematic
+# + addition
+# - substraction
+# * multipilcation
+# /division
+# %modulus
+# ** exponsentian
+# //floor divider
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**2)
+print(a//4) 
+# floor divider
+# It divides two numbers and returns the largest integer less than or equal to the result (i.e., it removes the decimal part).
+
+# assignment
