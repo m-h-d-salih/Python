1aa144a61c7dbeac0f21057c7cc5b6da2cfe8514
Mohammed Salih k <salihsha656@gmail.com>
2025-08-07 11:22:28 +0530

    arithematic

--- operators.py
+++ operators.py
@@ -20,3 +20,28 @@ print(a//4)
 # It divides two numbers and returns the largest integer less than or equal to the result (i.e., it removes the decimal part).
 
 # assignment
+a=10
+a+=4
+print(a)
+a-=4
+print(a)
+a*=4
+print(a)
+a/=4
+print(a)
+a%=4
+print(a)
+a+=10
+a//=4
+print(a)
+a**=4
+print(a)
+# comparison
+a=10
+b=20
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a >= b)
+print(a <= b)
