5ee33672964ea43eec5d6c109fe554b2bb9d8b1f
Mohammed Salih k <salihsha656@gmail.com>
2025-08-07 10:57:39 +0530

    basics

--- helloPython.py
+++ helloPython.py
@@ -1,4 +1,23 @@
+#python introduction
+
 print('hello world')
 print('python starting')
 print(1)
 print(10+20)
+
+#varibales and data types
+a=10
+b=3.2
+x=10j
+print(a)
+name="salih"
+print(name)
+print(type(a),type(name),type(b),type(x))
+# boolean
+python=True
+java=False
+print(type(python),type(java))
+a=b=c=10
+print(a,b,c)
+a,b,c="bugatti","mclaren","pagani"
+print(a,b,c)
