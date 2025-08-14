ddf825139798d8dd2d7b8e8dc54755993b74d113
Mohammed Salih k <salihsha656@gmail.com>
2025-08-14 13:43:48 +0530

    loop

---
+++ loops.py
@@ -0,0 +1,13 @@
+a="helllo"
+for i in a:
+    print(i)
+a=["helllo",1,2]
+for i in a:
+    print(i)
+for i in range(6): #limit only
+    print(i)
+
+for i in range(1,6): #starting point and limit
+    print(i)
+for i in range(1,6,2): #starting point , limit and updation value
+    print(i)
